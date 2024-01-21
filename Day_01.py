08ce2e13c7a2381b81a94dbb30386923f8e306ac
Scott <scott@linfords.com>
2024-01-21 05:10:47 -0800

    Part two passes

--- Day_01.py
+++ Day_01.py
@@ -1,5 +1,6 @@
 import re
 import unittest
+
 import numpy as np
 
 
@@ -22,30 +23,41 @@ def part_one(filename):
         left_digit = result.group(1)
         result = re.search(r'(\d)\D*$', row)
         right_digit = result.group(1)
-        row_value = int(left_digit+right_digit)
+        row_value = int(left_digit + right_digit)
         document_value += row_value
 
     return document_value
 
 
-DIGITS = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine',
-          '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+DIGIT_NAMES = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine',
+               '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+DIGIT_VALUES = {'zero': 0, 'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5, 'six': 6, 'seven': 7, 'eight': 8,
+                'nine': 9, '0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}
 
 
 def find_digits(row, digits):
-    indexes = np.zeros((len(digits), 2), dtype=int)
+    indexes = []
     for i, digit in enumerate(digits):
-        indexes[i][0] = row.find(digit)
-        indexes[i][1] = row.rfind(digit)
+        left_index = row.find(digit)
+        right_index = row.rfind(digit)
+        if left_index != -1 or right_index != -1:
+            indexes.append([digit, left_index, right_index])
     return indexes
 
 
 def part_two(filename):
     data = read_puzzle_input(filename)
+    document_value = 0
     document = parse_data(data)
     for row in document:
-        indexes = find_digits(row, DIGITS)
-    return -1
+        indexes = find_digits(row, DIGIT_NAMES)
+        indexes_sorted_left = np.array(sorted(indexes, key=lambda x: x[1]))
+        indexes_sorted_right = np.array(sorted(indexes, key=lambda x: x[2], reverse=True))
+        digit_left = indexes_sorted_left[0][0]
+        digit_right = indexes_sorted_right[0][0]
+        row_value = DIGIT_VALUES[digit_left] * 10 + DIGIT_VALUES[digit_right]
+        document_value += row_value
+    return document_value
 
 
 class Test(unittest.TestCase):
@@ -55,4 +67,5 @@ class Test(unittest.TestCase):
 
     def test_part_two(self):
         # self.assertEqual(part_two('Day_01_input.txt'), -1)
+        self.assertEqual(part_two('Day_01_input.txt'), 55701)
         self.assertEqual(part_two('Day_01_short_input_02.txt'), 281)
