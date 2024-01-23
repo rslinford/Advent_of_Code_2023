9e4d32c17f0f377c76b4a56b0640195030966e17
Scott <scott@linfords.com>
2024-01-22 17:05:30 -0800

    stubbed

---
+++ Day_07.py
@@ -0,0 +1,34 @@
+import unittest
+from dataclasses import dataclass
+
+
+def read_puzzle_input(filename):
+    with open(filename, 'r') as f:
+        data = f.read()
+    return data
+
+
+def parse_data(data: str):
+    return data.split('\n')
+
+
+def part_one(filename):
+    data = read_puzzle_input(filename)
+    races = parse_data(data)
+    return -1
+
+
+def part_two(filename):
+    data = read_puzzle_input(filename)
+    races = parse_data(data)
+    return -1
+
+
+class Test(unittest.TestCase):
+    def test_part_one(self):
+        self.assertEqual(-1, part_one('Day_07_input.txt'))
+        self.assertEqual(-1, part_one('Day_07_short_input.txt'))
+
+    def test_part_two(self):
+        self.assertEqual(-1, part_two('Day_07_input.txt'))
+        self.assertEqual(-1, part_two('Day_07_short_input.txt'))
