f3c32b2570f460c23b523f678f376db5be0eccc3
Scott <scott@linfords.com>
2024-01-23 01:05:40 -0800

    Day 8 part one passes

--- Day_08.py
+++ Day_08.py
@@ -19,21 +19,39 @@ class Node:
     right: str
 
 
-def parse_data(data: str) -> (str, list[Node]):
+def parse_data(data: str) -> (str, dict[str, Node]):
     data = data.split('\n\n')
     directions = data[0]
-    nodes = []
+    nodes = {}
     for row in data[1].split('\n'):
         result = re.search(r'(\w+) = \((\w+), (\w+)\)', row)
-        nodes.append(Node(result.group(1), result.group(2), result.group(3)))
+        nodes[result.group(1)] = Node(result.group(1), result.group(2), result.group(3))
     return directions, nodes
+
+
+def follow_directions(directions, nodes: dict[str, Node]):
+    steps_taken = 0
+    current_node = nodes['AAA']
+    while True:
+        direction = directions[steps_taken % len(directions)]
+        match direction:
+            case 'L':
+                current_node = nodes[current_node.left]
+            case 'R':
+                current_node = nodes[current_node.right]
+            case _:
+                assert False
+        steps_taken += 1
+        if current_node.name == 'ZZZ':
+            break
+    return steps_taken
 
 
 def part_one(filename):
     data = read_puzzle_input(filename)
     directions, nodes = parse_data(data)
-    print(nodes)
-    return -1
+    steps_taken = follow_directions(directions, nodes)
+    return steps_taken
 
 
 def part_two(filename):
@@ -44,9 +62,9 @@ def part_two(filename):
 
 class Test(unittest.TestCase):
     def test_part_one(self):
-        # self.assertEqual(-1, part_one('Day_08_input.txt'))
-        self.assertEqual(-1, part_one('Day_08_short_input.txt'))
-        # self.assertEqual(-1, part_one('Day_08_short_input_02.txt'))
+        self.assertEqual(18023, part_one('Day_08_input.txt'))
+        self.assertEqual(2, part_one('Day_08_short_input.txt'))
+        self.assertEqual(6, part_one('Day_08_short_input_02.txt'))
 
     def test_part_two(self):
         self.assertEqual(-1, part_two('Day_08_input.txt'))
