6483d1575149de3acccb95427197a3dd283611ba
Scott <scott@linfords.com>
2024-01-23 00:29:08 -0800

    Day 8 parsing complete

--- Day_08.py
+++ Day_08.py
@@ -12,14 +12,27 @@ def read_puzzle_input(filename):
     return data
 
 
-def parse_data(data: str):
-    data = data.split('\n')
-    return data
+@dataclass
+class Node:
+    name: str
+    left: str
+    right: str
+
+
+def parse_data(data: str) -> (str, list[Node]):
+    data = data.split('\n\n')
+    directions = data[0]
+    nodes = []
+    for row in data[1].split('\n'):
+        result = re.search(r'(\w+) = \((\w+), (\w+)\)', row)
+        nodes.append(Node(result.group(1), result.group(2), result.group(3)))
+    return directions, nodes
 
 
 def part_one(filename):
     data = read_puzzle_input(filename)
-    hands = parse_data(data)
+    directions, nodes = parse_data(data)
+    print(nodes)
     return -1
 
 
@@ -31,8 +44,9 @@ def part_two(filename):
 
 class Test(unittest.TestCase):
     def test_part_one(self):
-        self.assertEqual(-1, part_one('Day_08_input.txt'))
+        # self.assertEqual(-1, part_one('Day_08_input.txt'))
         self.assertEqual(-1, part_one('Day_08_short_input.txt'))
+        # self.assertEqual(-1, part_one('Day_08_short_input_02.txt'))
 
     def test_part_two(self):
         self.assertEqual(-1, part_two('Day_08_input.txt'))
