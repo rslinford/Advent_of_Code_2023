c8b47244c5283ddd02ad57edfe8a1a1bd6cf2f11
Scott <scott@linfords.com>
2024-01-23 21:14:20 -0800

    Day 11 inserting columns is complete

--- Day_11.py
+++ Day_11.py
@@ -10,14 +10,36 @@ def read_puzzle_input(filename):
 
 
 def parse_data(data: str):
-    data = [[m for m in n] for n in data.split('\n')]
-    data = np.array(data)
-    return data
+    data = [list(a) for a in data.split('\n')]
+    return np.array(data)
+
+
+def find_empty_columns(space):
+    columns = []
+    for x in range(space.shape[1]):
+        column_is_empty = True
+        for y in range(space.shape[0]):
+            if space[y][x] == '#':
+                column_is_empty = False
+                break
+        if column_is_empty:
+            columns.append(x)
+    return columns
+
+
+def expand_columns(space, columns):
+    columns.sort(reverse=True)
+    dots = list('@' * space.shape[0])
+    for column in columns:
+        space = np.insert(space, column, dots, axis=1)
+    return space
 
 
 def part_one(filename):
     data = read_puzzle_input(filename)
-    parse_data(data)
+    space = parse_data(data)
+    columns = find_empty_columns(space)
+    space = expand_columns(space, columns)
     return -1
 
 
@@ -29,7 +51,7 @@ def part_two(filename):
 
 class Test(unittest.TestCase):
     def test_part_one(self):
-        self.assertEqual(-1, part_one('Day_11_input.txt'))
+        # self.assertEqual(-1, part_one('Day_11_input.txt'))
         self.assertEqual(-1, part_one('Day_11_short_input.txt'))
 
     def test_part_two(self):
