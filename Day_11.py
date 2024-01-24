4728970261dbe65e898d16a2fded12d8183323d3
Scott <scott@linfords.com>
2024-01-23 21:38:03 -0800

    Day 11 inserting rows is complete

--- Day_11.py
+++ Day_11.py
@@ -27,19 +27,47 @@ def find_empty_columns(space):
     return columns
 
 
+def find_empty_rows(space):
+    rows = []
+    for y in range(space.shape[0]):
+        row_is_empty = True
+        for x in range(space.shape[1]):
+            if space[y][x] == '#':
+                row_is_empty = False
+                break
+        if row_is_empty:
+            rows.append(y)
+    return rows
+
+
 def expand_columns(space, columns):
     columns.sort(reverse=True)
-    dots = list('@' * space.shape[0])
+    dots = list('.' * space.shape[0])
     for column in columns:
         space = np.insert(space, column, dots, axis=1)
+    return space
+
+
+def expand_rows(space, rows):
+    rows.sort(reverse=True)
+    dots = list('.' * space.shape[1])
+    for row in rows:
+        space = np.insert(space, row, dots, axis=0)
+    return space
+
+
+def expand(space):
+    columns = find_empty_columns(space)
+    space = expand_columns(space, columns)
+    rows = find_empty_rows(space)
+    space = expand_rows(space, rows)
     return space
 
 
 def part_one(filename):
     data = read_puzzle_input(filename)
     space = parse_data(data)
-    columns = find_empty_columns(space)
-    space = expand_columns(space, columns)
+    space = expand(space)
     return -1
 
 
@@ -52,7 +80,7 @@ def part_two(filename):
 class Test(unittest.TestCase):
     def test_part_one(self):
         # self.assertEqual(-1, part_one('Day_11_input.txt'))
-        self.assertEqual(-1, part_one('Day_11_short_input.txt'))
+        self.assertEqual(374, part_one('Day_11_short_input.txt'))
 
     def test_part_two(self):
         self.assertEqual(-1, part_two('Day_11_input.txt'))
