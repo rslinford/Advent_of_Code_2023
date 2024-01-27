5e11c6742b771adc399a76f81b468424a44e476f
Scott <scott@linfords.com>
2024-01-26 16:02:37 -0800

    Day 16 stubbed

---
+++ Day_16.py
@@ -0,0 +1,33 @@
+import unittest
+
+
+def read_puzzle_input(filename):
+    with open(filename, 'r') as f:
+        data = f.read()
+    return data
+
+
+def parse_data(data: str):
+    return data.split(',')
+
+
+def part_one(filename):
+    data = read_puzzle_input(filename)
+    data = parse_data(data)
+    return -1
+
+
+def part_two(filename):
+    data = read_puzzle_input(filename)
+    data = parse_data(data)
+    return -1
+
+
+class Test(unittest.TestCase):
+    def test_part_one(self):
+        self.assertEqual(-1, part_one('Day_16_input.txt'))
+        self.assertEqual(-1, part_one('Day_16_short_input.txt'))
+
+    def test_part_two(self):
+        self.assertEqual(-1, part_two('Day_16_input.txt'))
+        self.assertEqual(-1, part_two('Day_16_short_input.txt'))
