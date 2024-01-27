5b97b30eeb775622d7d08d66ad2279f4dacf0615
Scott <scott@linfords.com>
2024-01-27 14:27:17 -0800

    Day 16 part one in progress

--- Day_16.py
+++ Day_16.py
@@ -44,7 +44,10 @@ def light_up_contraption(contraption):
     width, height = len(contraption[0]), len(contraption)
     light_map = [[0] * width for _ in range(height)]
     cursors = [Cursor(0, 0, Heading.RIGHT)]
-    while cursors:
+    new_cursors = []
+    while cursors or new_cursors:
+        cursors.extend(new_cursors)
+        new_cursors.clear()
         for cursor in cursors:
             light_map[cursor.y][cursor.x] += 1
             match contraption[cursor.y][cursor.x]:
@@ -71,9 +74,65 @@ def light_up_contraption(contraption):
                             else:
                                 cursors.remove(cursor)
                 case '|':
-                    pass
+                    match cursor.heading:
+                        case Heading.UP:
+                            if cursor.y > 0:
+                                cursor.y -= 1
+                            else:
+                                cursors.remove(cursor)
+                        case Heading.DOWN:
+                            if cursor.y < height - 1:
+                                cursor.y += 1
+                            else:
+                                cursors.remove(cursor)
+                        case Heading.LEFT:
+                            new_cursor = Cursor(cursor.x, cursor.y + 1, Heading.DOWN)
+                            if cursor.y > 0:
+                                cursor.y -= 1
+                                cursor.heading = Heading.UP
+                            else:
+                                cursors.remove(cursor)
+                            if cursor.y < height - 1:
+                                new_cursors.append(new_cursor)
+                        case Heading.RIGHT:
+                            new_cursor = Cursor(cursor.x, cursor.y + 1, Heading.DOWN)
+                            if cursor.y > 0:
+                                cursor.y -= 1
+                                cursor.heading = Heading.UP
+                            else:
+                                cursors.remove(cursor)
+                            if cursor.y < height - 1:
+                                new_cursors.append(new_cursor)
                 case '-':
-                    pass
+                    match cursor.heading:
+                        case Heading.UP:
+                            new_cursor = Cursor(cursor.x + 1, cursor.y, Heading.RIGHT)
+                            if cursor.x > 0:
+                                cursor.x -= 1
+                                cursor.heading = Heading.LEFT
+                            else:
+                                cursors.remove(cursor)
+                            if cursor.x < width - 1:
+                                new_cursors.append(new_cursor)
+                        case Heading.DOWN:
+                            new_cursor = Cursor(cursor.x + 1, cursor.y, Heading.RIGHT)
+                            if cursor.x > 0:
+                                cursor.x -= 1
+                                cursor.heading = Heading.LEFT
+                            else:
+                                cursors.remove(cursor)
+                            if cursor.x < width - 1:
+                                new_cursors.append(new_cursor)
+                        case Heading.LEFT:
+                            if cursor.x > 0:
+                                cursor.x -= 1
+                            else:
+                                cursors.remove(cursor)
+                        case Heading.RIGHT:
+                            if cursor.x < width - 1:
+                                cursor.x += 1
+                            else:
+                                cursors.remove(cursor)
                 case '/':
                     match cursor.heading:
                         case Heading.UP:
@@ -128,38 +187,15 @@ def light_up_contraption(contraption):
                                 cursors.remove(cursor)
                 case _:
                     assert False
-            match cursor.heading:
-                case Heading.UP:
-                    if cursor.y == 0:
-                        cursors.remove(cursor)
-                    else:
-                        cursor.y -= 1
-                case Heading.DOWN:
-                    if cursor.y == height - 1:
-                        cursors.remove(cursor)
-                    else:
-                        cursor.y += 1
-                case Heading.LEFT:
-                    if cursor.x == 0:
-                        cursors.remove(cursor)
-                    else:
-                        cursor.x -= 1
-                case Heading.RIGHT:
-                    if cursor.x == width - 1:
-                        cursors.remove(cursor)
-                    else:
-                        cursor.y += 1
-
-    print(light_map_to_string(light_map))
 
     return light_map
-
 
 
 def part_one(filename):
     data = read_puzzle_input(filename)
     contraption = parse_data(data)
-    light_up_contraption(contraption)
+    light_map = light_up_contraption(contraption)
+    print(light_map_to_string(light_map))
     print(contraption_to_string(contraption))
     return -1
 
@@ -172,9 +208,9 @@ def part_two(filename):
 
 class Test(unittest.TestCase):
     def test_part_one(self):
-        # self.assertEqual(-1, part_one('Day_16_input.txt'))
-        self.assertEqual(-1, part_one('Day_16_short_input.txt'))
+        # self.assertEqual(7939, part_one('Day_16_input.txt'))
+        self.assertEqual(46, part_one('Day_16_short_input.txt'))
 
     def test_part_two(self):
         self.assertEqual(-1, part_two('Day_16_input.txt'))
-        self.assertEqual(-1, part_two('Day_16_short_input.txt'))
+        self.assertEqual(51, part_two('Day_16_short_input.txt'))
