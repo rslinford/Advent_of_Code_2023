fe0315722e540bef9d1e24e1a53a784386466a17
Scott <scott@linfords.com>
2024-01-27 15:05:09 -0800

    Day 17 stubbed

---
+++ Day_17.py
@@ -0,0 +1,35 @@
+import unittest
+from dataclasses import dataclass
+from enum import Enum, auto
+
+
+def read_puzzle_input(filename):
+    with open(filename, 'r') as f:
+        data = f.read()
+    return data
+
+
+def parse_data(data: str):
+    return data.split('\n')
+
+
+def part_one(filename):
+    data = read_puzzle_input(filename)
+    data = parse_data(data)
+    return -1
+
+
+def part_two(filename):
+    data = read_puzzle_input(filename)
+    data = parse_data(data)
+    return -1
+
+
+class Test(unittest.TestCase):
+    def test_part_one(self):
+        self.assertEqual(-1, part_one('Day_17_input.txt'))
+        self.assertEqual(-1, part_one('Day_17_short_input.txt'))
+
+    def test_part_two(self):
+        self.assertEqual(-1, part_two('Day_17_input.txt'))
+        self.assertEqual(-1, part_two('Day_17_short_input.txt'))
