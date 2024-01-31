463e190decfb68d7f703c8276f295bbab9042e30
Scott <scott@linfords.com>
2024-01-31 00:03:04 -0800

    Day 21 part one passes

--- Day_21.py
+++ Day_21.py
@@ -8,25 +8,68 @@ def read_puzzle_input(filename):
 
 
 def parse_data(data: str):
-    return data.split('\n')
+    grid = [list(a) for a in data.split('\n')]
+    return grid
 
 
-def part_one(filename):
+def find_start(grid):
+    for y, row in enumerate(grid):
+        for x, col in enumerate(row):
+            if col == 'S':
+                return x, y
+    assert False
+
+
+DIRS = [(0, -1), (0, 1), (-1, 0), (1, 0)]
+
+
+def walk_a_step(grid, locations):
+    height = len(grid)
+    width = len(grid[0])
+    new_locations = set()
+    for x, y in locations:
+        for dx, dy in DIRS:
+            x2 = x + dx
+            y2 = y + dy
+            if x2 < 0 or x2 >= width or y2 < 0 or y2 >= height:
+                continue
+            if grid[y2][x2] == '.' or grid[y2][x2] == 'S':
+                new_locations.add((x2, y2))
+    return new_locations
+
+
+def print_grid(grid, locations):
+    for y, row in enumerate(grid):
+        for x, col in enumerate(row):
+            if (x, y) in locations:
+                print('O', end='')
+            else:
+                print(col, end='')
+        print()
+
+
+def part_one(filename, steps=64):
     data = read_puzzle_input(filename)
-    data = parse_data(data)
-    return -1
+    grid = parse_data(data)
+    locations = {find_start(grid)}
+    for n in range(steps):
+        locations = walk_a_step(grid, locations)
+        print(f'After {n + 1} steps')
+        print_grid(grid, locations)
+        print()
+    return len(locations)
 
 
 def part_two(filename):
     data = read_puzzle_input(filename)
-    data = parse_data(data)
+    grid = parse_data(data)
     return -1
 
 
 class Test(unittest.TestCase):
     def test_part_one(self):
-        self.assertEqual(-1, part_one('Day_21_input.txt'))
-        self.assertEqual(-1, part_one('Day_21_short_input.txt'))
+        self.assertEqual(3600, part_one('Day_21_input.txt'), 64)
+        # self.assertEqual(16, part_one('Day_21_short_input.txt', 6))
 
     def test_part_two(self):
         self.assertEqual(-1, part_two('Day_21_input.txt'))
