f5da059394334424fd2aa72438d7a5ad1ab54aae
Scott <scott@linfords.com>
2024-02-03 13:23:57 -0800

    learning material

--- Day_10_scratch.py
+++ Day_10_scratch.py
@@ -1,78 +1,109 @@
-NORTH = (0, -1)
-EAST = (1, 0)
-SOUTH = (0, 1)
-WEST = (-1, 0)
+def findNext(cur, x, y):
+    if cur == "7":
+        return [[x - 1, y], [x, y + 1]]
+    elif cur == "J":
+        return [[x - 1, y], [x, y - 1]]
+    elif cur == "F":
+        return [[x + 1, y], [x, y + 1]]
+    elif cur == "L":
+        return [[x + 1, y], [x, y - 1]]
+    elif cur == "-":
+        return [[x - 1, y], [x + 1, y]]
+    elif cur == "|":
+        return [[x, y + 1], [x, y - 1]]
 
 
-def reverse_dir(dir: tuple[int, int]):
-    return dir[0] * -1, dir[1] * -1
+with open("Day_10_input.txt") as f:
+    pipes = f.read().splitlines()
 
+for i in range(len(pipes)):
+    if "S" in pipes[i]:
+        start = [pipes[i].index('S'), i]
 
-Point = tuple[int, int]
-Grid = list[list[str]]
+stepmap = [['.'] * len(pipe) for pipe in pipes]
+steps, curQueue, nextQueue = 1, [], []
 
+# Part 2 to get the S into its proper shape
+shapeFlag = 0
+shapeMap = {11: "-", 101: "J", 1001: "7", 110: "L", 1010: "F", 1100: "|"}
 
-def padd(a: Point, b: Point) -> Point:
-    return a[0] + b[0], a[1] + b[1]
+if pipes[start[1]][start[0] - 1] in ["F", "L", "-"]:
+    shapeFlag += 1
+    curQueue.append((start[0] - 1, start[1]))
+if pipes[start[1]][start[0] + 1] in ["J", "7", "-"]:
+    shapeFlag += 10
+    curQueue.append((start[0] + 1, start[1]))
+if pipes[start[1] - 1][start[0]] in ["F", "7", "|"]:
+    shapeFlag += 100
+    curQueue.append((start[0], start[1] - 1))
+if pipes[start[1] + 1][start[0]] in ["J", "L", "|"]:
+    shapeFlag += 1000
+    curQueue.append((start[0], start[1] + 1))
 
+stepmap[start[1]][start[0]] = shapeMap[shapeFlag]
 
-def gget(g: Grid, p: Point) -> str:
-    x, y = p[0], p[1]
-    if (x < 0) or (x >= len(g[0])) or (y < 0) or (y >= len(g)):
-        return '.'
-    return g[y][x]
+while curQueue:
+    x, y = curQueue.pop()
+    stepmap[y][x] = pipes[y][x]
+    nextSteps = findNext(pipes[y][x], x, y)
+    for step in nextSteps:
+        if stepmap[step[1]][step[0]] == ".":
+            nextQueue.append(step)
 
+    if not curQueue:
+        curQueue = nextQueue
+        nextQueue = []
+        steps += 1
 
-def connections(tile: str):
-    if tile == '-':
-        return EAST, WEST
-    if tile == '|':
-        return NORTH, SOUTH
-    if tile == 'F':
-        return EAST, SOUTH
-    if tile == 'L':
-        return NORTH, EAST
-    if tile == 'J':
-        return NORTH, WEST
-    if tile == '7':
-        return SOUTH, WEST
-    print("unhandled tile encountered: ", tile)
-    assert False  # uh-oh, something wrong!
+newMap, newlines = [], []
+for j, line in enumerate(stepmap):
+    i = 0
 
+    newline = []
+    while i < len(line):
+        if line[i] in ["L", "F", "-"]:
+            line.insert(i + 1, "-")
+        else:
+            line.insert(i + 1, ".")
+        if line[i] in ["F", "|", "7"]:
+            newline += ["|", "."]
+        else:
+            newline += [".", "."]
+        i += 2
+    newlines.append(newline)
 
-def solve(start: Point, grid: Grid):
-    visited = [start]
-    next_dirs = {NORTH, SOUTH, EAST, WEST}
-    while True:
-        for d in next_dirs:
-            n = padd(visited[-1], d)
-            # see if it's a valid tile we can travel to
-            tile = gget(grid, n)
-            if tile == '.':
-                continue
-            if tile == 'S':
-                return visited
-            if reverse_dir(d) in connections(tile):
-                # yes, we can travel here
-                next_dirs = set(connections(tile))
-                next_dirs.remove(reverse_dir(d))
-                visited.append(n)
-                break
+bigmap = [None] * (len(stepmap) * 2)
+bigmap[::2] = stepmap  # I accidentally stumbled into this lol
+bigmap[1::2] = newlines
+bigmap.insert(0, ["."] * len(bigmap[0]))
 
+curQueue = [[0, 0]]
+while curQueue:
+    x, y = curQueue.pop()
+    bigmap[y][x] = "O"
+    try:
+        if bigmap[y][x - 1] == ".":
+            curQueue.append([x - 1, y])
+    except:
+        pass
+    try:
+        if bigmap[y][x + 1] == ".":
+            curQueue.append([x + 1, y])
+    except:
+        pass
+    try:
+        if bigmap[y + 1][x] == ".":
+            curQueue.append([x, y + 1])
+    except:
+        pass
+    try:
+        if bigmap[y - 1][x] == ".":
+            curQueue.append([x, y - 1])
+    except:
+        pass
 
-def part1(start: Point, grid: Grid):
-    path = solve(start, grid)
-    print(f"Part 1: {len(path) / 2}")
+# for line in stepmap:
+#    print(" ".join([str(x) for x in line[::2]]))
 
-
-if __name__ == '__main__':
-    with open('Day_10_short_input_03.txt') as f:
-        data = f.read()
-    print(data)
-    grid = [list(line) for line in data.splitlines()]
-    start = None
-    for (y, row) in enumerate(grid):
-        if 'S' in row:
-            start = (row.index('S'), y)
-            break
-    part1(start, grid)
+print(steps - 1)
+print(sum(line[::2].count(".") for line in stepmap))
