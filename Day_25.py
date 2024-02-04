e70c850894691be91871fa9684ae235c52b0ee74
Scott <scott@linfords.com>
2024-02-04 00:02:11 -0800

    Part one passes

--- Day_25.py
+++ Day_25.py
@@ -1,7 +1,6 @@
+import random
 import unittest
-
-import networkx as nx
-from matplotlib import pyplot as plt
+from collections import defaultdict, deque
 
 
 def read_puzzle_input(filename):
@@ -11,11 +10,12 @@ def read_puzzle_input(filename):
 
 
 def parse_data(data: str):
-    connections = []
+    connections = defaultdict(set)
     for row in data.splitlines():
         a, b = row.split(': ')
         for c in b.split(' '):
-            connections.append((a, c))
+            connections[a].add(c)
+            connections[c].add(a)
     return connections
 
 
@@ -29,28 +29,39 @@ xtx, njn
 
 def part_one(filename):
     data = read_puzzle_input(filename)
-    connections = parse_data(data)
-    g = nx.Graph()
-    for con in connections:
-        g.add_edge(con[0], con[1])
-    # nx.draw(g, with_labels=True)
-    # plt.show()
-    cv, p = nx.stoer_wagner(g)
-    result = len(p[0]) * len(p[1])
+    graph = parse_data(data)
+    inbetween_count = defaultdict(int)
+    for node in graph:
+        q = deque([(node, [])])
+        visited = {node}
+        while q:
+            current_node, path = q.popleft()
+            for edge in path:
+                inbetween_count[edge] += 1
+            for nxt in graph[current_node]:
+                if nxt not in visited:
+                    visited.add(nxt)
+                    edge = tuple(sorted([current_node, nxt]))
+                    q.append((nxt, path + [edge]))
+    busy_edges = sorted(inbetween_count, key=inbetween_count.get)[-3:]
+    for node1, node2 in busy_edges:
+        graph[node1].remove(node2)
+        graph[node2].remove(node1)
+
+    node = random.choice(list(graph.keys()))
+    q = deque([node])
+    visited = set()
+    while q:
+        node = q.popleft()
+        if node not in visited:
+            visited.add(node)
+            for nxt in graph[node]:
+                q.append(nxt)
+    result = len(visited) * (len(graph) - len(visited))
     return result
-
-
-def part_two(filename):
-    data = read_puzzle_input(filename)
-    data = parse_data(data)
-    return -1
 
 
 class Test(unittest.TestCase):
     def test_part_one(self):
-        self.assertEqual(-1, part_one('Day_25_input.txt'))
+        self.assertEqual(558376, part_one('Day_25_input.txt'))
         self.assertEqual(54, part_one('Day_25_short_input.txt'))
-
-    def test_part_two(self):
-        self.assertEqual(-1, part_two('Day_25_input.txt'))
-        self.assertEqual(-1, part_two('Day_24_short_input.txt'))
---
+++ Day_25_scratch.py
@@ -0,0 +1,58 @@
+# I re-did this one without the help of NetworkX, to see if I could and surprisingly it runs just as fast
+
+from collections import defaultdict, deque
+
+with open('Day_25_input.txt', 'r') as f:
+    puzzle_input = f.read()
+
+
+def part1(puzzle_input):
+    # Construct graph
+    graph = defaultdict(list)
+    # edges = set()
+    for line in puzzle_input.split('\n'):
+        node1, connected = line.split(': ')
+        for node2 in connected.split():
+            graph[node1].append(node2)
+            graph[node2].append(node1)
+            # edges.add(tuple(sorted((node1, node2))))
+
+    # Calculate edge betweenness centrality
+    edge_betweenness = defaultdict(int)
+    for node in graph:
+        visited = {node}
+        queue = deque([(node, [])])
+        while queue:
+            for _ in range(len(queue)):
+                curr, path = queue.popleft()
+                for edge in path:
+                    edge_betweenness[edge] += 1
+                for nxt in graph[curr]:
+                    if nxt not in visited:
+                        visited.add(nxt)
+                        edge = tuple(sorted((nxt, curr)))
+                        queue.append((nxt, path + [edge]))
+
+    # Remove the three most frequently travelled edges from graph
+    most_crucial_edges = sorted(edge_betweenness, key=edge_betweenness.get)[-3:]
+    for node1, node2 in most_crucial_edges:
+        graph[node1].remove(node2)
+        graph[node2].remove(node1)
+
+    # Traverse one of the resulting smaller graphs to find out its size
+    visited = {node}
+    queue = [node]
+    while queue:
+        curr = queue.pop()
+        for nxt in graph[curr]:
+            if nxt not in visited:
+                visited.add(nxt)
+                queue.append(nxt)
+
+    # Infer graph sizes and return their product
+    size1 = len(visited)
+    size2 = len(graph) - size1
+    return size1 * size2
+
+
+print('Part 1:', part1(puzzle_input))
